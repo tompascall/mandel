b874fc6b837bea4b1cad90b04938ed33a32f1e97
tompascall <tompascall@gmail.com>
2014-10-10 22:57:51 +0200

    handle ie8 problems

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 // index.js
 
 "use strict";
+$(document).ready(function(){
 
-if (Modernizr.canvas){
-  mandel.init();
-}
-else {
-  mandelUI.setDisplay("panel", "none");
-}
+  if (Modernizr.canvas && Modernizr.webworkers){
+    mandelUI.setDisplay("panel", "inline-block");
+    mandelUI.setDisplay("mandelCanvas", "inline-block");
+    mandelUI.setDisplay("canvasNotSupported", "none");
+    mandel.init();
+  };
+});
+
 
 
 
